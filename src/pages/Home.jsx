67db178a3dd9d714426e3430b827e59dd046cd9e
a-nihad <a.nihadkarulai@gmail.com>
2024-06-18 21:05:17 +0530

    Update hame page image URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,15 +10,15 @@ function Home() {
 
   return (
     <div>
-      <header className="flex h-[280px] w-full flex-col bg-[url('coffee.jpg')] bg-cover bg-center object-cover sm:h-[400px] md:h-[480px]">
+      <header className="flex h-[280px] w-full flex-col bg-[url('/public/coffee.jpg')] bg-cover bg-center object-cover sm:h-[400px] md:h-[480px]">
         {!isAuthenticated && (
           <div className="px-10 py-20 sm:py-28 md:px-20 md:py-40">
             <h1 className="text-color_white py-2 text-2xl sm:py-4 sm:text-3xl md:py-8 md:text-6xl">
               Welcome to Coffee Hub
             </h1>
             <Button
-              variation="primary"
               onClick={() => navigate("/login")}
+              variation="primary"
               className="rounded-md px-6 py-1 md:px-14 md:py-2 md:text-xl"
             >
               Login
